a6bb6b8a4fcdcc9c4e5a302a0befdf56f7dee956
Alexander Kosykh <sanykos446@gmail.com>
2022-01-16 14:31:27 +0300

    Updates

---
+++ precache-manifest.7ebac71acbe27bbf70314d476b4f7b0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab0a1fb96e9164399b1e67f39b71003",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "ed161538741815006061",
+    "url": "/react-todo/static/css/2.a389d3b1.chunk.css"
+  },
+  {
+    "revision": "20812726c1b4e5d7ad65",
+    "url": "/react-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed161538741815006061",
+    "url": "/react-todo/static/js/2.3cf87041.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/react-todo/static/js/2.3cf87041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20812726c1b4e5d7ad65",
+    "url": "/react-todo/static/js/main.fc197b38.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
