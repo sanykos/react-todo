7110741ca4a009275b879d0a0743d0d7f0615c7a
Alexander Kosykh <sanykos446@gmail.com>
2022-07-13 17:19:11 +0300

    add e2e

--- tests/e2e/todos.e2e.js
+++ tests/e2e/todos.e2e.js
@@ -4,7 +4,9 @@ describe('My todo add ', () => {
   it('should enter title', async () => {
     await TodoPage.open();
 
-    await TodoPage.addTodo('Title');
+    await TodoPage.addTodo('You are late for 15 minutes, Bitch');
+    // await TodoPage.a
+    // await driver.TodoPag
     // await expect(SecurePage.flashAlert).toBeExisting();
     // await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!');
   });
--- tests/pages/todo.page.js
+++ tests/pages/todo.page.js
@@ -1,3 +1,4 @@
+// const { OpenInBrowser } = require('@material-ui/icons');
 const Page = require('./page');
 
 class TodoPage extends Page {
@@ -7,6 +8,7 @@ class TodoPage extends Page {
 
   async addTodo(title) {
     await this.inputAddTodo.setValue(title);
+    await browser.keys('Enter');
   }
 
   open() {
